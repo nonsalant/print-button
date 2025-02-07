22defb12638b0ed04f0e7ff5e6cc97dff4bc2371
Stefan Matei <nonsalant@gmail.com>
2025-02-07 15:28:46 +0200

    add script and readme

--- README.md
+++ README.md
@@ -1,2 +1,71 @@
 # print-button
- Web Component to print only a specific element on the page (instead of the whole page)
+ Web Component to print only a specific element instead of the whole page (which is what `window.print()` does by default).
+
+**[CodePen Demo](https://codepen.io/nonsalant/pen/yyBmeBp)**
+
+## Examples
+
+Print a specific element on the page:
+```html
+<print-button print-target=".element-to-print">
+    Print
+</print-button>
+```
+Print the whole page:
+```html
+<print-button>Print</print-button>
+```
+
+Multiple print buttons on the same page:
+```html
+<print-button print-target="#table-1">
+    Print the 1st table
+</print-button>
+<table id="table-1">...</table>
+
+<print-button print-target="#table-2">
+    Print the 2nd table
+</print-button>
+<table id="table-2">...</table>
+```
+
+Just the icon:
+```html
+<print-button print-target=".element-to-print"></print-button>
+```
+
+## Attributes and Content
+- `print-target` attribute: CSS selector of the element to print. By default (if not provided), it prints the whole page except for the button itself.
+- Content: The text content of the button. If left empty, `aria-label="Print"` is added to the button (which will be a simple icon button).
+
+## Including the script
+
+The script can be included anywhere on the page... 
+- as a module:
+```html
+<script type="module" src="print-button.js"></script>
+```
+- or as a non-module script tag:
+```html
+<script src="print-button.js"></script>
+```
+
+## How it works
+The `printOnly()` method:
+- starts at the target, 
+- adds a `.dont-print` class to its siblings, 
+- moves to the parent and adds the class to _its_ siblings, 
+- and continues up the DOM recursively adding the `.dont-print` class to all of the "uncles" (non-ancestors) of the target element.
+
+## Other notes
+The `setTimeout(()=>{...})` wrapping the insides of the `connectedCallback()` lifecycle method allows the script to be used as a non-module script tag (in addition to the module option) from anywhere on the page, including the header, by adding a ~4ms delay which should be enough for the DOM to be ready. <br>Increase the delay if needed (if your `print-target` takes longer to load), e.g:
+```javascript
+connectedCallback() {
+    setTimeout(() => {
+        //...
+    }, 150);
+}
+```
+
+## Credits
+- Print icon from [Ionicons 5: print-sharp](https://github.com/ionic-team/ionicons/blob/main/src/svg/print-sharp.svg).
---
+++ print-button.js
@@ -0,0 +1,100 @@
+class PrintElement extends HTMLElement {
+    buttonTemplate() {
+        // Styles can be moved to global CSS
+        const buttonStyles = `display: flex; gap: .25em; align-items: center;`;
+        const svgStyles = `width: 1em; height: 1em;`;
+
+        const svg = `
+			<svg style="${svgStyles}" viewBox="0 0 512 512" stroke="currentColor" fill="currentColor" stroke-width="0">
+				<path d="M400 96V56a8 8 0 0 0-8-8H120a8 8 0 0 0-8 8v40"></path><rect width="208" height="160" x="152" y="264" fill="none" rx="4" ry="4"></rect><path d="M408 112H104a56 56 0 0 0-56 56v208a8 8 0 0 0 8 8h56v72a8 8 0 0 0 8 8h272a8 8 0 0 0 8-8v-72h56a8 8 0 0 0 8-8V168a56 56 0 0 0-56-56zm-48 308a4 4 0 0 1-4 4H156a4 4 0 0 1-4-4V268a4 4 0 0 1 4-4h200a4 4 0 0 1 4 4zm34-212.08a24 24 0 1 1 22-22 24 24 0 0 1-22 22z"></path>
+			</svg>
+        `;
+
+        // If label is not provided, hide it and use aria-label
+        const label = this.label ? `<span>${this.label}</span>` : '';
+        const ariaLabel = this.label ? '' : `aria-label="Print"`;
+
+        return `
+			<button style="${buttonStyles}" ${ariaLabel}>
+				${svg}
+				${label}
+			</button>
+        `;
+    }
+
+    constructor() {
+        super();
+    }
+    
+    connectedCallback() {
+        setTimeout(() => {
+            const selector = this.getAttribute('print-target');
+            this.printTarget = document.querySelector(selector);
+            this.label = this.innerHTML;
+            this.innerHTML = this.buttonTemplate();
+            this.attachEvent();
+        });
+    }
+
+    disconnectedCallback() {
+        this.clearDontPrintClasses();
+        this.removeGlobalStyles();
+    }
+
+    attachEvent() {
+        this.querySelector('button').addEventListener('click', () => {
+            this.addGlobalStyles();
+            this.clearDontPrintClasses();
+
+            const el = this.printTarget;
+            if (!el) {
+                // Just add the class to the button if printing the whole page
+                this.classList.add('dont-print');
+            } else {
+                this.printOnly(el);
+            }
+
+            window.print(); // 🖨️
+        });
+    }
+
+    clearDontPrintClasses() {
+        document.querySelectorAll('.dont-print').forEach(el => {
+            el.classList.remove('dont-print');
+        });
+    }
+
+    addGlobalStyles() {
+        // Add global CSS for print styling if not already added
+        if (!document.querySelector('#dont-print-style')) {
+            const style = document.createElement('style');
+            style.id = 'dont-print-style';
+            style.textContent = `.dont-print { @media print { display: none !important; } }`;
+            document.head.appendChild(style);
+        }
+    }
+
+    removeGlobalStyles() {
+        document.querySelector('#dont-print-style').remove();
+    }
+
+    printOnly(element) {
+        let currentElement = element;
+        while (currentElement) {
+            if (!currentElement.parentNode) break;
+            let sibling = currentElement.parentNode.firstChild;
+            while (sibling) {
+                if (sibling !== currentElement) {
+                    if (sibling.nodeType === 1) // ensure it's an element
+                        sibling.classList.add('dont-print');
+                }
+                sibling = sibling.nextSibling;
+            }
+            // Move to the parent element
+            currentElement = currentElement.parentNode;
+        }
+    }
+
+}
+
+customElements.define('print-button', PrintElement);
